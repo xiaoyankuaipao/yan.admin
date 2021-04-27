f515fe95ef5626fc023fb95f5f9f2ddda7cd7316
xiaoyankuaipao <511657675@qq.com>
2021-04-27 20:44:46 +0800

    Update index.vue

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-      <el-table ref="table" border fit="fit" :stripe="stripe" :data="tableData" :empty-text="emptyText" highlight-current-row style="width:100%">
-
+      <el-table ref="table" border fit="fit" :stripe="stripe" :data="tableData" :empty-text="emptyText" highlight-current-row style="width:100%"
+        @selection-change="onSelectionChange" @current-change="onCurrentRowChange">
+        <slot name="columns"></slot>
       </el-table>
+      <div v-if="pagination" class="Pagination" style="text-align: left;margion-top: 10px;">
+        <el-pagination v-if="reload" @size-change="handleSizeChanged" @current-change="handleCurrentPageChanged" :current-page="currentPage"
+          :page-sizes="pageSize" :page-size="limit" layout="total, -> ,sizes, -> , prev, pager, next" :total="count">
+        </el-pagination>
+      </div>
   </div>
 </template>
 
@@ -70,6 +76,70 @@ export default {
       emptyText: 'Sorry,暂无数据',
       // 选中行
       selectRow: null
+    }
+  },
+  created () {
+    this.initData()
+  },
+  methods: {
+    async getApiData () {
+      var params = this.params
+      if (this.pagination) {
+        params.page = this.currentPage
+        params.rows = this.limit
+      }
+      this.emptyText = '加载中……'
+      this.tableData = []
+      this.reload = false
+      const result = await this.searchApi(params)
+      if (result.state === 1) {
+        this.tableData = result.result.data
+        this.count = result.result.totalCount
+        if (this.count.length === 0) {
+          this.emptyText = '暂无数据……'
+        }
+        this.reload = true
+      } else {
+        this.$message.error('获取数据失败！')
+      }
+    },
+    async initData () {
+      try {
+        this.getApiData()
+      }
+      catch (err) {
+        this.$message.error('获取数据失败！')
+      }
+    },
+    handleSizeChanged(val) {
+      this.limit = val
+      this.getApiData()
+    },
+    handleCurrentPageChanged(val) {
+      this.currentPage = val
+      this.offset = (val - 1) * this.limit
+      this.getApiData()
+    },
+    onCurrentRowChange(selection) {
+      this.selectRow = selection
+    },
+    onSelectionChange(selection) {
+      this.selectRow = selection
+    },
+    getSelectRow() {
+      this.selectRow
+    },
+    getTableData() {
+      return this.tableData
+    },
+    search(parmas) {
+      for ( var i in parmas) {
+        this.params[i] = parmas[i]
+       }
+       this.getApiData()
+    },
+    clearSelection() {
+      this.$refs.table.clearSelection()
     }
   }
 }
